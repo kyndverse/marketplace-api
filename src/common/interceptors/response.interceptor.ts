af6eb22a24bfacddc734bcd67821f35ab771953a
IhsanJati <ihsanjati127@gmail.com>
2026-04-10 13:20:55 +0700

    feat: add ResponseInterceptor to format API responses

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { map, Observable } from 'rxjs';
+import { ApiResponse } from 'src/model/response.model';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<
+  ApiResponse<T>,
+  any
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<ApiResponse<T>>,
+  ): Observable<any> {
+    const response = context.switchToHttp().getResponse<Response>();
+    return next.handle().pipe(
+      map((res) => ({
+        code: response.statusCode,
+        status: HttpStatus[response.statusCode],
+        message: res.message,
+        data: res.data,
+        meta: res.meta,
+      })),
+    );
+  }
+}
